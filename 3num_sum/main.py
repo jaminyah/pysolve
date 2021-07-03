f8306ebbd0feca8bdbe17ab4cba1d4c15f84c876
jaminyah <jaminyah@gmail.com>
2021-07-03 01:02:08 -0400

    Add solution for 3 number sum problem.

---
+++ 3num_sum/main.py
@@ -0,0 +1,50 @@
+# Algo expert success July 2, 2021
+
+from typing import List
+
+class Solution:
+    def threeSum(self, a: List[int], target: int) -> List[List[int]]:
+    
+            """
+            i - first index
+            j - second index
+            k - third index
+            """
+
+            output = []
+            a.sort()
+            length = len(a)
+
+            for i in range(0, length):
+
+                # increment if next element == previous element           
+                if i > 0 and a[i] == a[i - 1]:
+                    continue
+
+                j = i + 1
+                k = length - 1
+                element = a[i]
+                remainder  = target - element
+
+                while j < k:
+                    sum = a[j] + a[k]
+                    if sum == remainder:
+                        elements = [a[i], a[j], a[k]]
+                        output.append(elements)                         # append elements 
+                        j = j + 1
+                        # increment if next element == previous element
+                        while j < k and a[j] == a[j - 1]:
+                            j = j + 1
+                    elif sum < remainder:
+                        j = j + 1
+                    else:                                               # sum > remainder:
+                        k = k - 1
+
+            return output
+
+
+if __name__ == '__main__':
+    #array = [0, 1, 0, 2, -1, -2]
+    array = [12, 3, 1, 2, -6, 5, 0, -8, -1, 6]
+    target = 0
+    print(Solution().threeSum(array, target))
